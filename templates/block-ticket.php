48c38b45bcb276a73de226806b818dbde21b9ebd
Pip <zapstartdigital@gmail.com>
2026-06-27 01:36:25 +0100

    Add block-ticket.php template for ticket display

---
+++ templates/block-ticket.php
@@ -0,0 +1,17 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+<div class="ets-ticket-block" id="<?php echo esc_attr( $block_id ); ?>"
+     data-ets-rest-url="<?php echo esc_attr( $rest_url ); ?>"
+     data-ets-stripe-pk="<?php echo esc_attr( $publishable_key ); ?>">
+    <?php
+    if ( $ticket_style === 'cards' ) {
+        include ETS_PLUGIN_DIR . 'templates/ticket-layout-cards.php';
+    } else {
+        include ETS_PLUGIN_DIR . 'templates/ticket-layout-table.php';
+    }
+    ?>
+</div>
+<script src="https://js.stripe.com/v3/"></script>
